f2bc5648e9b4d8d861a5f5c2e1429f7b2033cf2b
BRYAN1309 <bryanthanaya1@gmail.com>
2024-07-20 21:11:31 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import PokemonPage from './pages/PokemonPage';
 import SearchPage from './pages/SearchPage';
